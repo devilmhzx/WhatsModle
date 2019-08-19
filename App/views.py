193028623ed89a8bd6b089f176bfeb64c854207e
devilmhzx <241681095@qq.com>
2019-08-19 13:48:26 +0800

    Django8月19日 实现增删改查

--- App/views.py
+++ App/views.py
@@ -12,19 +12,30 @@ def index(request):
 
 def addGoos(request):
     g = Goods()
-    g.gname = '商品'+str(int(time.time()))
-
+    g.gname = '商品' + str(int(time.time()))
+    g.ginfo = '此条是卖家通过路由创建的商品'
     g.save()
-    return HttpResponse('OK')
+    return HttpResponse(g.gname + '创建成功！')
 
 
 def delGoos(request):
-    return HttpResponse('OK')
+    g = Goods.objects.last()  # 删除最后一个商品
+    g.delete()
+    return HttpResponse(g.gname + '删除成功!')
 
 
+# 改
 def modGoos(request):
-    return HttpResponse('OK')
+    g = Goods.objects.last()
+    g.ginfo = g.gname + '修改后的！'
+    g.save()
+    return HttpResponse(g.gname + '修改成功')
 
 
+# 查
 def getGoos(request):
-    return HttpResponse('OK')
+    gs = Goods.objects.filter(gprice__gt=0)
+    rt = ''
+    for g in gs:
+        rt += (g.gname + ';')
+    return HttpResponse(rt)
