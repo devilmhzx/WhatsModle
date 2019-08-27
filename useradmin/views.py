c9d14213d6415cead9a4290ab9c8f3ade23d24a5
devilmhzx <241681095@qq.com>
2019-08-27 15:00:08 +0800

    Django8月26日 实现Cookie记录用户信息

--- useradmin/views.py
+++ useradmin/views.py
@@ -1,4 +1,7 @@
-from django.http import JsonResponse
+from datetime import timedelta
+
+from django.core.serializers import json
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -28,6 +31,7 @@ def register(request):
 
 def login(request):
     JsonData = {'returnJson': '登录失败', 'states': ['已退出', '已登陆']}
+    httpRe = HttpResponse()
     if request.method == 'GET':
         return render(request, 'Login.html')
     else:
@@ -38,13 +42,31 @@ def login(request):
         if user and upwd == user.upwd:
             JsonData['returnJson'] = '登录成功'
             JsonData['states'] = '已登录'
-        ReturnJson = JsonResponse(JsonData)
-        return ReturnJson
+            # 通过客户端(cookie)保存用户状态
+            # httpRe.set_cookie('uname', uname, max_age=60)
+            httpRe.set_cookie('uname', uname, expires=timedelta(days=1))
+            # 让服务端通过session来保存用户状态
+
+        httpRe.content = JsonData.items()
+        # ReturnJson = JsonResponse(JsonData)
+
+        return httpRe
 
 
 def logout(request):
-    return None
+    httpRe = HttpResponse()
+    # 让客户端删除保存的用户状态
+    httpRe.delete_cookie('uname')
+    # 删除服务端session
+
+    httpRe.content = '登出成功'
+    return httpRe
 
 
 def mine(request):
-    return None
+    # 从COOKIE中获取用户状态
+    uname = request.COOKIES.get('uname', None)
+    # 从服务端session获取用户状态
+
+    uname = uname if uname != None else '你还未登录'
+    return HttpResponse('你好，' + uname)
